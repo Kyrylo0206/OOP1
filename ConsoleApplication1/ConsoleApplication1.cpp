7001170135c1396c866a605dfda61841ead2972b
Kyrylo Shykhalov <kshykhalov@kse.org.ua>
2024-09-05 13:02:33 +0300

    Add first class Airplane

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,20 +1,55 @@
 ﻿#include <iostream>
+#include <string>
+#include <vector>
 #pragma once 
 using namespace std;
 
 class Airplane {
 private:
-
+    int total_seats = 204;
+    vector <bool> availability_seat;
 public:
-    void do();
+	Airplane(int seats) : total_seats(seats), availability_seat(seats, true) {}
+	//check is place free
+	bool SeatAvailable(int seat_number) {
+		if (seat_number < 0 || seat_number > total_seats) {
+			cout << "Invalid seat number.\n";
+			return false;
+		return availability_seat  [seat_number - 1]; 
+		}
+	}
+	bool Booking (int seat_number) {
+		if (SeatAvailable(seat_number)) {
+			availability_seat[seat_number - 1] = false;
+			return true;
+		}
+		return false;
+	}
+	int getTotalSeats() const {
+		return total_seats;
+	}
 
 };
 class Ticket {
+private:
+    void booking_status(char check);
 
+public:
+   
 };
 
 int main()
 {
-    cout << "Hello World!\n";
+	int c;
+
+	string name;
+
+	cout << "Welcome to Airline Reservations System!\n";
+	cout << "Please enter your name: ";
+	getline(cin, name);
+
+	cout << "Choose a class: ";
+	cin >> c;
+
 }
 
