52e8ad8b4c0c3481528102175b37feed33fb9a1b
Kyrylo Shykhalov <kshykhalov@kse.org.ua>
2024-09-10 12:22:38 +0300

    add boking function

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -5,6 +5,26 @@
 #pragma once 
 using namespace std;
 
+class User {
+	string UserName;
+	vector<Ticket> tickets;
+
+public:
+	User(const string& UserName) : UserName(UserName) {}
+
+	string GetUserName() const {
+		return UserName;
+	}
+
+	vector<Ticket>& UserTickets() {
+		return tickets;
+	}
+
+	void addTicket(const Ticket& ticket) {
+		tickets.push_back(ticket);
+	}
+};
+
 class Ticket {
 private:
 	string UserName;
@@ -64,16 +84,6 @@ public:
 			cout << "Ticket ID: " << ID << "\nPassenger: " << UserName << "\nFlight: " << FlightName << "\nSeat: " << Place
 				<< "\nPrice: $" << Price << "\nStatus: " << (IsBooked ? "Booked" : "Cancelled") << "\n";
 		}
-
-		void SetID(int newID) { 
-			ID = newID; 
-		}
-		void SetUserName(const string& name) { 
-			UserName = name; IsBooked = false; 
-		}
-		void DeleteBooking() { 
-			UserName = "-"; IsBooked = false; 
-		}
 	};
 
 class Airplane {
@@ -96,35 +106,41 @@ public:
 		}
 		return SeatAvailable;
 	}
-	bool Booking (int seats) {
-		if (SeatAvailable(seats)) {
-			availability_seat[seats - 1] = false;
-			return true;
-		}
-		return false;
+	void Booking(const string& UserName, const string& Place, int& ID, vector<Ticket>* IsBookedTickets, vector<User>* users) {
+		for (auto& ticket : tickets) {
+			if (ticket.GetPlace() == Place && ticket.GetIsBooked()) {
+				ticket.SetUserName(UserName);
+				ticket.SetID(ID);
+				IsBookedTickets->push_back(ticket);
+				bool UserFound = false;
+				for (auto& user : *users) {
+					if (user.GetUserName() == UserName) {
+						user.addTicket(ticket);
+						UserFound = true;
+						break;
+					}
+				}
+				if (!UserFound) {
+					User newUser(UserName);
+					newUser.addTicket(ticket);
+					users->push_back(move(newUser));
+				}
+				cout << "Confirmed with ID " << ID << endl;
+				return;
+			}
+		
 	}
+	void ReturnTicketByID(const int ticketId) {
+		for (auto& ticket : tickets) {
+			if (ticket.GetID() == ticketId) {
+				ticket.DeleteBooking();
+				return;
+			}
+		}
+	}
+
 	int getTotalSeats() const {
 		return total_seats;
-	}
-};
-
-class User {
-	string UserName;
-	vector<Ticket> tickets;
-
-public:
-	User(const string& UserName) : UserName(UserName) {}
-
-	string GetUserName() const {
-		return UserName;
-	}
-
-	vector<Ticket>& UserTickets() {
-		return tickets;
-	}
-
-	void addTicket(const Ticket& ticket) {
-		tickets.push_back(ticket);
 	}
 };
 
@@ -169,4 +185,3 @@ int main()
 }
 
 }
-
